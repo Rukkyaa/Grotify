bf8ce6ead8de7baa647a4d14fb650f01b1903235
Axel LAMBERT <axel.lambert@lgra-cournot.etab.local>
2021-10-19 14:55:42 +0200

    Fonction watch to embed + iframe depuis la base de données

--- accueil.php
+++ accueil.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 #session_destroy();
+$bdd = new SQLite3('db_grotify.db');
+
+function urlToEmbed($urlYTB){
+
+    $urlYTB = str_replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/", $urlYTB);
+    echo $urlYTB;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -31,7 +39,28 @@ session_start();
                     </nav>
                 <?php ;}?>
         </header>
+        <?php
+            $stmt = $bdd->prepare("SELECT music_url FROM Music INNER JOIN Artist ON Artist.artist_id = Music.artist_id WHERE Artist.name= :name");
+            $stmt->bindValue(":name", "Wejdene", SQLITE3_TEXT);
+            $req = $stmt->execute();?>
 
+            <div class = "video">
+            <?php
+            while ($donnees = $req->fetchArray())
+			{
+			    ?>
+                    <iframe width="420" height="315"
+                        src="<?php echo $donnees['music_url']?>">
+                    </iframe> 
+                    <?php ;
+			}?>
+
+            <?php 
+                urlToEmbed("https://www.youtube.com/watch?v=jdRE5jJUWhg")
+            ?>
+
+
+            </div>
         <!--
         <div class = "video">
             <iframe width="420" height="315"
@@ -41,7 +70,7 @@ session_start();
             <iframe width="420" height="315"
                 src="https://www.youtube.com/embed/GIJ3bL5AtQw">
             </iframe>   
-        </div>
-                -->
+        </div>-->
+
     </body>
 </html>
